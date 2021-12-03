87343d1200e5a643b53e29ca38780f2be24f7e14
Leandro Antunez <leandro_antunez@outlook.com>
2021-12-03 07:44:36 -0300

    Images from the books can be added.

--- admin/sections/products.php
+++ admin/sections/products.php
@@ -11,7 +11,17 @@ switch($accion) {
     case "Agregar":
         $sentenciaSQL= $conexion->prepare("INSERT INTO libros (nombre, imagen) VALUES (:nombre, :imagen);");
         $sentenciaSQL->bindParam(':nombre',$txtNombre);
-        $sentenciaSQL->bindParam(':imagen',$txtImagen);
+        
+        $fecha= new DateTime();
+        $nombreArchivo = ($txtImagen!="")?$fecha->getTimestamp()."_".$_FILES["txtImagen"]["name"]:"imagen.jpg";
+        
+        $tmpImagen=$_FILES["txtImagen"]["tmp_name"];
+
+        if ($tmpImagen!="") {
+            move_uploaded_file($tmpImagen, "../../img/".$nombreArchivo);
+        }
+        
+        $sentenciaSQL->bindParam(':imagen',$nombreArchivo);
         $sentenciaSQL->execute();
         break;
 
